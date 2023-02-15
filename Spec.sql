817f89a823278ca1d2c5303b65bb1206a53745cb
puahisrael <puahisrael1@gmail.com>
2023-02-14 21:55:46 -0600

    added the spec

---
+++ Spec.sql
@@ -0,0 +1,74 @@
+/*
+I am overwhelmed! Like up to my nose in stress! And you - yes YOU - have  the power to restore my peace of mind. With just a few clicks of the mouse...
+
+You see, about half a year ago, I held a small, informal yard sale in front of my house. It was wildly successful - beyond anything I could've hoped for -and 
+immediately after that initial event, I recieved a multitude of requests to  run a second sale. So I did... and then I did it again... and again... until my house
+was practically rid of all its junk (and admittedly some non-junk items as well). Well, then the donations started pouring in. Because once word got around that 
+my yard was a veritable junk yard, everyone had the brilliant idea to drop off all of their unused stuff... because it's a mess anyway, right? 
+
+So that's how that one-time spur-of-the-moment venture became a weekly sale in an abandoned lot in my neighborhood.
+
+Well I can't really complain because ultimately everyone else's garbage has turned me into a business woman. Running a real business. (With a real name - "Not A Dump"). And making real money.
+But still, it's incredibly chaotic, and I cannot for the life of me keep track of what's coming in and what's going out, and probably many people are walking off 
+with free items simply because the prices are not being tracked.
+
+So, yeah. Stress.
+
+But you can help me!
+Because you can record all of the necessary data in a neat database.
+
+So here goes...
+
+I need to keep track of every single item that passes through my yard, and record its name, description, and the category of "stuff" that it falls under.
+The main categories are as follows:
+1) Home
+2) Recreation 
+3) Equipment
+4) Clothing
+5) Misc
+
+I also need to record the date that the item was put out and the date it was sold (if applicable). 
+And, of course, the price needs to be tracked.
+
+Also, I recently hired a teenager from the neighborhood to help with some of the organization and set-up. She then gets 20% of the earnings.
+So I need to record her salary, as well as the take-home pay for myself.
+
+Questions:
+Q: Have you ever bought anything specifically for the sale?
+A: No! The donations that I receive and the stuff that I already have are the ONLY items which are put out. Therefore, it's all profit...
+
+Q: Have you ever given away anything for free?
+A: Um...yes. Because of the mess, there are sometimes items lying around which weren't intentionally meant to be put out, and are therefore not tagged.
+   Usually, they are too old or ragged for me to charge the buyer in good consience, and I let them take it for no cost.
+
+Q: Do you need to record the donors?
+A: Yes! I forgot to mention this part. If someone gives at least 3 donations (not 3 items, 3 separate drop-offs on 3 separate dates), they receive a complementary
+   "Not A Dump" t-shirt! (This gesture is being supported by a kind neighbor and is NOT coming from my own pocket.) 
+
+Reports:
+1) For advertising purposes, I need to know which items I should be aiming to receive. Therefore, show me a list of the categories ordered by popularity (most popular on the top).
+2) I want to upgrade the price tags. Each category should have it's own color sticker (you can choose), and the text should be formatted as such: item name (category name): price, date put out
+   (I want the color and the text in separate columns. And just show me a list, please, as a preview of what they would look like.)
+3) I am considering reducing the price of all items that are still not sold after 3 months of sitting there. Show me a 50% reduction on all such items.
+4) The "t-shirt" neighbor wants to customize the shirts! Show me a list of the "worthy" donors and their t-shirts, formatted as: "Not A Dump", Thanks to YOU, <donor name>!
+
+This is a very small sampling of my inventory, I want to test your competency before I send you more data:
+Tricycle, Red and blue; heavily used but still functional, Recreation, Tom Smith, 11/27/2022, null, $15
+Scarf, Women's; hardly worn, Clothing, Jane Jones, 05/22/2022, 06/14/2022, $5
+Dresser, 5-drawer; slightly shaky but very good condition, Home, Jane Jones, 01/04/2023, null, $35
+Guitar, missing string; small, Recreation, Sam Williams, 07/18/2022, 07/25/2022
+Lawn-mower, very old model but works, Equipment, Ted Richardson, 08/01/2022, null, $20
+Wallet, Men's; fake leather, Misc, Bob Mill, 06/09/2022, 08/15/2022, $3
+Lamp, pretty new-looking, Home, Jane Jones, 01/16/2023, null, $15
+Children's book, classic, Recreation, Tom Smith, 10/15/2022, 10/29/2022, $2
+
+
+
+
+
+
+
+
+
+
+*/
